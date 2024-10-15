3e4d1b650274cf6726360126ef6960bc5d39ff2e
unknown <BT_3W2_19@BKTECH.LOCAL>
2024-10-15 10:46:59 -0400

    Second Message

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.getElementById("count-el").innerText = 5
+// Shushana Saki Counter App
+
+let clickCount = 0
+function increment() { // This is a function
+    let countJS = document.getElementById("count-el")
+    clickCount++
+    console.log(clickCount)
+    countJS.innerHTML = clickCount
+    console.log(countJS)
+}
