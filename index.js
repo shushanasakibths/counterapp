2e2db658caf1886703e11b54731466732d21e4f6
Shushana Saki <sample@bths.edu>
2024-10-17 10:23:11 -0400

    fourht commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // Shushana Saki Counter App
 
 let clickCount = 0
+let savedEntries = "";
+
 function increment() { // This is a function
     let countJS = document.getElementById("count-el")
     clickCount++
     console.log(clickCount)
     countJS.innerHTML = clickCount
     console.log(countJS)
+}
+
+function save() {
+    let entriesEl = document.getElementById("entries");
+    savedEntries += clickCount + " - ";
+    entriesEl.textContent = "Previous entries: " + savedEntries;
+    document.getElementById("count-el").innerHTML = clickCount;
+
 }
